ff603ef9b287639bddb62d9b800839d4b8e9242e
bbb博 <120379739@qq.com>
2022-08-22 16:38:40 +0800

    增加注释

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  //集群部署时，API需要加上mall-admin路径。
+  //集群部署时，API需要加上mall-admin路径
   BASE_API: '"http://172.16.33.161:8201/mall-admin"'
 })
